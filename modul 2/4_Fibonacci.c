673c7acb3979af3501191ff905844ba18b01cf05
khrisnapriana <100006677+khrisnapriana@users.noreply.github.com>
2022-05-16 11:15:24 +0800

    Add files via upload

--- modul 2/4_Fibonacci.c
+++ modul 2/4_Fibonacci.c
@@ -4,25 +4,35 @@
 void judulprogram(){
 	printf("\n============================================\n");
 	printf("||    PROGRAM DERET BILANGAN FIBONACCI    ||\n");
-	printf("||       METODE REKURSIF & ITERATIF       ||\n"); 
+	printf("||       METODE REKURSIF & ITERATIF       ||\n");
+	printf("||              KELOMPOK 20               ||\n"); 
 	printf("============================================\n\n");
 }
 
 void judulrekursif(){
 	printf("\n========================================\n");
-	printf("||            Metode Rekursif         ||\n");
+	printf("||          Metode Rekursif           ||\n");
 	printf("||Menampilkan Deret Bilangan Fibonacci||\n");
+	printf("||            Kelompok 20             ||\n"); 
 	printf("========================================\n\n");
 }
 
-int deretfibonacci(int deret) {
+void juduliteratif(){
+	printf("\n========================================\n");
+	printf("||          Metode Iteratif           ||\n");
+	printf("||Menampilkan Deret Bilangan Fibonacci||\n");
+	printf("||            Kelompok 20             ||\n"); 
+	printf("========================================\n\n");
+}
+
+int fibonacci_rekursif(int deret) {
     if(deret == 0 || deret == 1){
     	//jika value deret = 0 atau 1, maka akan di-return value itu sendiri
     	return deret; 
 	}
     else{
     	//jika selain itu, maka akan di-return dengan rumus dibawah ini
-    	return deretfibonacci(deret-1) + deretfibonacci(deret-2);
+    	return fibonacci_rekursif(deret-1) + fibonacci_rekursif(deret-2);
     	//misal value deret = 4
     	//deret 0 = 0
     	//deret 1 = 1
@@ -31,11 +41,57 @@ int deretfibonacci(int deret) {
 	}    
 }
 
+void fibonacci_iteratif(int jumlahderet){
+	int deret1 = 0;
+	int	deret2 = 1;
+   	int deret = 0;
+   	int deretn;
+   	
+	do{
+	    //variabel deretn akan berubah-ubah setiap kali perulangan do-while berjalan
+	    //1. deretn = 0 + 1 = 1
+	    //2. deretn = 1 + 1 = 2
+	    //3. deretn = 1 + 2 = 3
+	    deretn = deret1 + deret2;
+	    if(deret == 0){
+	   		printf("Deret ke-1: %d \n", deret1);
+	    }
+	   	else if(deret == 1){
+	    	printf("Deret ke-2: %d \n", deret2);
+		}
+		else{
+			printf("Deret ke-%d: %d \n", deret+1, deretn);
+			//inisialisasi variabel deret1 dengan value deret2
+			deret1 = deret2;
+			//inisialisasi variabel deret2 dengan value deretn
+			deret2 = deretn;
+		}
+		deret++;
+	}
+	while(deret<jumlahderet);
+}
+
+int validasi(){
+	int angka;
+	char karakter;
+	
+	if(scanf("%d%c",&angka,&karakter)!= 2 || karakter != '\n'){
+		//Untuk menghilangkan nilai buffer pada system
+		//Sehingga saat melakukan input kembali, nilai sebelumnya tidak mempengaruhi input selanjutnya
+		fflush(stdin);	
+		printf("\nHanya menerima input berupa angka saja!\n");
+		printf("Masukkan input yang benar: ");
+		return validasi();
+	}
+	else{
+		return angka;
+	}
+}
+
 int main(){
     int i, menu;
     int banyakderet;
-    char menu_salah = 'y';
-    int deret1, deret2, deret, deretn;
+    char menu_salah = 'y' || 'Y';
     
     do{
     	//memanggil fungsi judulprogram
@@ -47,57 +103,29 @@ int main(){
 		printf("||3. Keluar             ||\n");
 		printf("==========================\n\n");
 		printf("Masukkan pilihan menu: ");	
-		scanf("%d", &menu);
+		menu = validasi();
 		//clear layar setelah user menginputkan menu
 		system("cls");
 	    
 	    if(menu == 1){
-	    	//memanggil fungsi judulrekursif
-		    judulrekursif();
-		   	printf("Masukkan jumlah deret: ");
-		   	scanf("%d", &banyakderet);
-		    system("cls");
-		    judulrekursif();
-		    printf("Deret bilangan fibonacci: \n\n");
-		    //perulangan untuk menampilkan deret fibonacci
-		   	for(i = 0; i<banyakderet; i++){
-		       	printf("Deret ke-%d: %d \n", i+1, deretfibonacci(i));
-		   	}  
+		    //memanggil fungsi judulrekursif
+			judulrekursif();
+			printf("Masukkan jumlah deret: ");
+			banyakderet = validasi();
+			printf("Deret bilangan fibonacci: \n\n");
+			//perulangan untuk menampilkan deret fibonacci
+			for(i=0; i<banyakderet; i++){
+			    printf("Deret ke-%d: %d \n", i+1, fibonacci_rekursif(i));
+			}  
 		}
 		else if(menu == 2){
-			//inisialisasi variabel untuk metode iteratif
-    		deret1 = 0;
-			deret2 = 1;
-   			deret = 0;
-			printf("\n========================================\n");
-			printf("||            Metode Iteratif         ||\n");
-			printf("||Menampilkan Deret Bilangan Fibonacci||\n");
-			printf("========================================\n\n");
+			//memanggil fungsi juduliteratif
+			juduliteratif();
 			printf("Masukkan jumlah deret: ");
-	    	scanf("%d", &banyakderet);
-	    	printf("\nDeret bilangan fibonacci: \n\n");
-	    	do{
-	    		//variabel deretn akan berubah-ubah setiap kali perulangan do-while berjalan
-	    		//1. deretn = 0 + 1 = 1
-	    		//2. deretn = 1 + 1 = 2
-	    		//3. deretn = 1 + 2 = 3
-	    		deretn = deret1 + deret2;
-	    		if(deret == 0){
-	    			printf("Deret ke-1: %d \n", deret1);
-	    		}
-	    		else if(deret == 1){
-	    			printf("Deret ke-2: %d \n", deret2);
-				}
-				else{
-					printf("Deret ke-%d: %d \n", deret+1, deretn);
-					//inisialisasi variabel deret1 dengan value deret2
-					deret1 = deret2;
-					//inisialisasi variabel deret2 dengan value deretn
-					deret2 = deretn;
-				}
-			deret++;
-			}
-			while(deret<banyakderet);
+			banyakderet = validasi();
+			printf("\nDeret bilangan fibonacci: \n\n");
+			//memanggil fungsi fibonacci_iteratif
+			fibonacci_iteratif(banyakderet);
 		}
 		else if(menu == 3){
 			//fungsi untuk keluar dari program
@@ -105,13 +133,15 @@ int main(){
 		}
 		else{
 			judulprogram();
-			printf("Input yang anda masukkan tidak ada dalam menu! Silahkan coba lagi");
+			printf("Input tidak ada dalam menu! Silahkan coba lagi");
 		}
-		printf("\n\nKembali ke menu? (y/t): ");
-		scanf(" %c", &menu_salah);
+		printf("\n========================================\n\n");
+		printf("*Tekan selain keyword Y/y untuk keluar*\n");
+		printf("Kembali ke menu? (Y/y): ");
+		scanf("%c", &menu_salah);
 		system("cls");
-	}
-    while(menu_salah == 'y');
+	}  
+    while(menu_salah == 'y' || menu_salah == 'Y');
     
     return 0;
 }
